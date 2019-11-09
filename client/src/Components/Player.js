c760cc41222423566ffd08a5cb4cfab340a59002
Brian Hague <alphaseinor@gmail.com>
2019-11-08 21:15:24 -0600

    removed test for players, and added player with prop data passe din

---
+++ client/src/Components/Player.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Player (props){
+  console.log(props)
+  return(<div>hithere</div>)
+}
+
+export default Player;
--- client/src/Components/Players.js
+++ client/src/Components/Players.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import axios from 'axios'
-
+import Player from './Player.js'
 
 class Players extends React.Component {
   state = {
-    player: {}
+    players: []
   }
 
   componentDidMount = () => {
     axios.get("http://localhost:5000/api/players")
          .then(result =>{
-           console.log(result)
+           //console.log(result)
+           this.setState({
+             players: result.data
+           })
          })
+         .catch(err => console.log(err))
   }
 
   render(){
+    const {players} = this.state;
     return(
-      <>
-      </>
+      <div>
+        {players.map(player => {
+          return <Player player={player} />
+        })}
+      </div>
     )
   }
 
